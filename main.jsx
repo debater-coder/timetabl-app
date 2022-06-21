370271c62f3f1d879ff8b02d57987de63ed0f8e2
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-06-22 08:47:43 +1000

    fix: nuke the service worker

--- main.jsx
+++ main.jsx
@@ -23,15 +23,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ChakraProvider>
   </React.StrictMode>
 );
-
-// Init service worker
-if ("serviceWorker" in navigator) {
-  console.log("CLIENT: service worker registration in progress.");
-
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(() => console.log("CLIENT: service worker registration complete."))
-    .catch(() => console.log("CLIENT: service worker registration failure."));
-} else {
-  console.log("CLIENT: service worker is not supported.");
-}
