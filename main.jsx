677eb6ce8d4154641b8d6cd3017d8f37070ce3c2
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-06-21 10:03:23 +1000

    Better service worker

--- main.jsx
+++ main.jsx
@@ -26,6 +26,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 // Init service worker
 if ("serviceWorker" in navigator) {
-  // noinspection JSIgnoredPromiseFromCall
-  navigator.serviceWorker.register("/sw.js");
+  console.log("CLIENT: service worker registration in progress.");
+
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(() => console.log("CLIENT: service worker registration complete."))
+    .catch(() => console.log("CLIENT: service worker registration failure."));
+} else {
+  console.log("CLIENT: service worker is not supported.");
 }
