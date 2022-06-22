ceec663d9a4e69d76cc2359b4f92080a04dfd3c1
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-06-22 15:22:34 +1000

    fix(sw): finally fix sw for dev and prod (hopefully)

--- sw.js
+++ sw.js
@@ -4,7 +4,4 @@ console.log("Your custom service worker code");
 
 if (typeof is_vite_preview === "undefined") {
   precacheAndRoute(self.__WB_MANIFEST);
-  console.log("precache!");
-} else {
-  console.log("skipping precache in dev");
 }
