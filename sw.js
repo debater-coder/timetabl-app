6aadd02b851873de6aa24d86b2aa0ac8be12c67a
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-06-22 16:08:22 +1000

    feat(sw): add StaleWhileRevalidate

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute, Route } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
 
-console.log("Your custom service worker code");
+const shellRoute = new Route(({ request, sameOrigin }) => {
+  return sameOrigin && request.method === "GET";
+}, new StaleWhileRevalidate());
+
+registerRoute(shellRoute);
 
 if (typeof is_vite_preview === "undefined") {
   precacheAndRoute(self.__WB_MANIFEST);
