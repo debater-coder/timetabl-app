4a92f5f93e1769f32f46fb5b5436ab860667cb56
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-06-28 21:40:53 +1000

    fix(sw): respond to non-root urls

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-import { precacheAndRoute } from "workbox-precaching";
+import { createHandlerBoundToURL, precacheAndRoute } from "workbox-precaching";
+import { NavigationRoute, registerRoute } from "workbox-routing";
 
 if (typeof is_vite_preview === "undefined") {
   precacheAndRoute(self.__WB_MANIFEST);
+  const handler = createHandlerBoundToURL("/app-shell.html");
+  const navigationRoute = new NavigationRoute(handler);
+  registerRoute(navigationRoute);
 }
