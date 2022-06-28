d26061c2e9a001cedf1a954995ae1b19c156767d
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-06-28 21:45:59 +1000

    fix(sw): correct precached uri

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ import { NavigationRoute, registerRoute } from "workbox-routing";
 
 if (typeof is_vite_preview === "undefined") {
   precacheAndRoute(self.__WB_MANIFEST);
-  const handler = createHandlerBoundToURL("/app-shell.html");
+  const handler = createHandlerBoundToURL("/index.html");
   const navigationRoute = new NavigationRoute(handler);
   registerRoute(navigationRoute);
 }
