a1ca98a2ab369887bb0e28c0eed1b5e32f77a11f
Hamzah Ahmed <hamzah@syedahmed.net>
2022-07-29 12:47:34 +1000

    feat: Dark Mode Toggle

--- routes/Main/Settings/Settings.jsx
+++ routes/Main/Settings/Settings.jsx
@@ -1,5 +1,28 @@
 import { Heading } from "@chakra-ui/react";
+import {
+  Switch,
+  Flex,
+  FormControl,
+  FormLabel,
+  useColorMode,
+} from "@chakra-ui/react";
 
 export default () => {
-  return <Heading>Settings</Heading>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Flex direction={"column"} width="full" textAlign={"center"} p={5}>
+      <Heading>Settings</Heading>
+      <FormControl display="flex" alignItems="center">
+        <FormLabel htmlFor="dark-mode-switch" mb="0">
+          Dark mode
+        </FormLabel>
+        <Switch
+          id="dark-mode-switch"
+          onChange={toggleColorMode}
+          isChecked={colorMode === "dark"}
+        />
+      </FormControl>
+    </Flex>
+  );
 };
