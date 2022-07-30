c0e51c7f6fa536192f019d0d9ff8ddb2ca2fffd7
Hamzah Ahmed <hamzah@syedahmed.net>
2022-07-30 18:36:16 +1000

    feat: upgrade to `tanstack/query`

--- main.jsx
+++ main.jsx
@@ -7,8 +7,8 @@ import registerSW from "./registerSW";
 import { Compose, withProps } from "./utils/contextualise";
 import { AuthProvider } from "./hooks/useAuth";
 import Routes from "./components/Routes";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
