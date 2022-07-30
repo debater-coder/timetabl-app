4841d21d3a4c6d5adc71cb2876bea3c2778caa6d
Hamzah Ahmed <hamzah@syedahmed.net>
2022-07-30 19:46:35 +1000

    feat: instant load

--- main.jsx
+++ main.jsx
@@ -7,10 +7,22 @@ import registerSW from "./registerSW";
 import { Compose, withProps } from "./utils/contextualise";
 import { AuthProvider } from "./hooks/useAuth";
 import Routes from "./components/Routes";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Compose
@@ -18,7 +30,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       StrictMode,
       withProps(ChakraProvider, { theme }),
       AuthProvider,
-      withProps(QueryClientProvider, { client: queryClient }),
+      withProps(PersistQueryClientProvider, {
+        client: queryClient,
+        persistOptions: { persister },
+      }),
     ]}
   >
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
