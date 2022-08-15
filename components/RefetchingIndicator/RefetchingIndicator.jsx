795439de21c773e7b899a7f75df7ddfbc1a5052f
Hamzah Ahmed <hamzah@syedahmed.net>
2022-08-16 09:25:28 +1000

    feat: add offline indicator

--- components/RefetchingIndicator/RefetchingIndicator.jsx
+++ components/RefetchingIndicator/RefetchingIndicator.jsx
@@ -4,14 +4,24 @@ import {
   Spinner,
   Text,
   IconButton,
+  Icon,
 } from "@chakra-ui/react";
-import { useIsFetching, useQueryClient } from "@tanstack/react-query";
+import {
+  onlineManager,
+  useIsFetching,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { AnimatePresence, motion } from "framer-motion";
-import { ArrowClockwise } from "phosphor-react";
+import { ArrowClockwise, WifiX } from "phosphor-react";
+import { useState } from "react";
 
 export default () => {
   const queryClient = useQueryClient();
   const isFetching = useIsFetching();
+  const [online, setOnline] = useState(true);
+  const [hovering, setHovering] = useState(false);
+
+  onlineManager.subscribe(() => setOnline(onlineManager.isOnline()));
 
   return (
     <Flex
@@ -26,9 +36,28 @@ export default () => {
       right={8}
       as={motion.div}
       layout
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
     >
       <AnimatePresence>
-        {isFetching ? (
+        {online ? (
+          isFetching ? (
+            <Text
+              as={motion.p}
+              layout
+              initial={{ width: 0, scaleX: 0, opacity: 0 }}
+              animate={{ width: "auto", scaleX: 1, opacity: 1 }}
+              exit={{ width: 0, scaleX: 0, opacity: 0 }}
+              fontSize={"xs"}
+              fontFamily="Poppins, sans-serif"
+              whiteSpace={"nowrap"}
+            >
+              Fetching the latest data
+            </Text>
+          ) : (
+            ""
+          )
+        ) : hovering ? (
           <Text
             as={motion.p}
             layout
@@ -39,24 +68,28 @@ export default () => {
             fontFamily="Poppins, sans-serif"
             whiteSpace={"nowrap"}
           >
-            Fetching the latest data
+            No internet.
           </Text>
         ) : (
           ""
         )}
       </AnimatePresence>
-      {isFetching ? (
-        <Spinner size={"xs"} as={motion.div} layout />
+      {online ? (
+        isFetching ? (
+          <Spinner size={"xs"} as={motion.div} layout />
+        ) : (
+          <IconButton
+            as={motion.button}
+            layout
+            icon={<ArrowClockwise />}
+            size="xs"
+            variant={"ghost"}
+            rounded="full"
+            onClick={() => queryClient.refetchQueries()}
+          />
+        )
       ) : (
-        <IconButton
-          as={motion.button}
-          layout
-          icon={<ArrowClockwise />}
-          size="xs"
-          variant={"ghost"}
-          rounded="full"
-          onClick={() => queryClient.refetchQueries()}
-        />
+        <Icon as={WifiX} />
       )}
     </Flex>
   );
