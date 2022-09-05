7d9f2fdc36096072269635930dffe437f38555d6
Hamzah Ahmed <hamzah@syedahmed.net>
2022-09-05 12:25:39 +1000

    feat: display name and room

--- routes/Main/Home/Home.jsx
+++ routes/Main/Home/Home.jsx
@@ -1,17 +1,34 @@
-import { Flex, Skeleton, Text } from "@chakra-ui/react";
+import { Flex, Heading, Skeleton, Text } from "@chakra-ui/react";
 import QueryError from "../../../components/QueryError";
 import useSBHSQuery from "../../../hooks/useSBHSQuery";
 import { withProps } from "../../../utils/contextualise";
 import handleQuery from "../../../utils/handleQuery";
+import "@fontsource/poppins";
 
-const Bell = ({ bell, timetable }) => (
-  <Flex p={3} m={1} bg={"gray"} rounded={10} direction={"column"}>
-    <Text key={bell.bell}>{bell["bellDisplay"]}</Text>
-    <Text key={bell.bell}>
-      {timetable["timetable"]?.["periods"]?.[bell["bell"]]?.["room"] ?? ""}
-    </Text>
-  </Flex>
-);
+const Bell = ({ bell, timetable }) => {
+  const period = timetable["timetable"]?.["periods"]?.[bell["bell"]];
+  let name = bell["bellDisplay"];
+
+  if (period?.["title"]) {
+    name = period["title"];
+
+    if (period["year"]) {
+      name = period["year"] + name;
+      name = timetable["subjects"]?.[name]["title"] ?? name;
+    }
+  }
+
+  return (
+    <Flex p={3} m={1} bg={"gray"} rounded={10} direction={"column"}>
+      <Heading key={bell.bell} size="sm" fontFamily={"Poppins, sans-serif"}>
+        {name}
+      </Heading>
+      <Text key={bell.bell}>
+        {period?.["room"] ? `Room: ${period?.["room"]}` : ""}
+      </Text>
+    </Flex>
+  );
+};
 
 const HomeView = (isLoaded, data) => (
   <Skeleton isLoaded={isLoaded} rounded={5}>
