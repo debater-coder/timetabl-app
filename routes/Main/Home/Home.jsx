5e54a51ffc801fdfaa0183e037a9b02d0a84c171
Hamzah Ahmed <hamzah@syedahmed.net>
2022-09-05 18:14:02 +1000

    feat: really attractive home page

--- routes/Main/Home/Home.jsx
+++ routes/Main/Home/Home.jsx
@@ -14,8 +14,8 @@ import { withProps } from "../../../utils/contextualise";
 import handleQuery from "../../../utils/handleQuery";
 import "@fontsource/poppins";
 
-const Bell = ({ bell, timetable }) => {
-  const period = timetable["timetable"]?.["periods"]?.[bell["bell"]];
+const Bell = ({ bell, timetable, isLoaded }) => {
+  const period = timetable?.["timetable"]?.["periods"]?.[bell["bell"]];
   let name = bell["bellDisplay"];
   let subject = null;
   let teacher = period?.["teacher"];
@@ -25,7 +25,7 @@ const Bell = ({ bell, timetable }) => {
 
     if (period["year"]) {
       name = period["year"] + name;
-      subject = timetable["subjects"]?.[name] ?? subject;
+      subject = timetable?.["subjects"]?.[name] ?? subject;
       name = subject["title"] ?? name;
     }
   }
@@ -35,56 +35,58 @@ const Bell = ({ bell, timetable }) => {
   }
 
   return (
-    <Flex
-      m={1}
-      rounded={10}
-      _hover={{ bg: useToken("colors", "gray.400") + "22" }}
-      shadow="lg"
-    >
-      <Box
-        w={2}
-        roundedLeft={10}
-        bg={subject?.["colour"] ? `#${subject?.["colour"]}` : "transparent"}
-      />
-      <Flex direction={"column"} p={3} w="full">
-        <Flex gap={6}>
-          <Heading size="sm" fontFamily={"Poppins, sans-serif"}>
-            {name}
-          </Heading>
-          <Spacer />
-          <Text fontWeight={"semibold"}>
-            {period?.["room"] ?? bell?.["startTime"] ?? ""}
-          </Text>
+    <Skeleton rounded={5} m={1} isLoaded={isLoaded}>
+      <Flex
+        m={1}
+        rounded={10}
+        _hover={{ bg: useToken("colors", "gray.400") + "22" }}
+        shadow="lg"
+      >
+        <Box
+          w={2}
+          roundedLeft={10}
+          bg={subject?.["colour"] ? `#${subject?.["colour"]}` : "transparent"}
+        />
+        <Flex direction={"column"} p={3} w="full">
+          <Flex gap={6}>
+            <Heading size="sm" fontFamily={"Poppins, sans-serif"}>
+              {name}
+            </Heading>
+            <Spacer />
+            <Text fontWeight={"semibold"}>
+              {period?.["room"] ?? bell?.["startTime"] ?? ""}
+            </Text>
+          </Flex>
+          {period?.["room"] && (
+            <Text fontWeight={"semibold"}>
+              {bell?.["startTime"]} {teacher ?? ""}
+            </Text>
+          )}
         </Flex>
-        {period?.["room"] && (
-          <Text fontWeight={"semibold"}>
-            {bell?.["startTime"]} {teacher ?? ""}
-          </Text>
-        )}
       </Flex>
-    </Flex>
+    </Skeleton>
   );
 };
 
-const HomeView = (isLoaded, data) => (
-  <Skeleton isLoaded={isLoaded} rounded={5}>
+const HomeView = (isLoaded, data) => {
+  const bells = data?.["bells"] ?? Array(11).fill({});
+  return (
     <Flex
       direction={"column"}
       bg={useToken("colors", useColorModeValue("gray.300", "gray.500")) + "33"}
       rounded={10}
     >
-      {data?.["bells"] && data?.["timetable"]
-        ? data["bells"].map((bell) => (
-            <Bell
-              key={bell["bell"]}
-              bell={bell}
-              timetable={data?.["timetable"]}
-            />
-          ))
-        : ""}
+      {bells.map((bell) => (
+        <Bell
+          key={bell["bell"]}
+          bell={bell}
+          timetable={data?.["timetable"]}
+          isLoaded={isLoaded}
+        />
+      ))}
     </Flex>
-  </Skeleton>
-);
+  );
+};
 
 export default () => {
   const { data, error } = useSBHSQuery("timetable/daytimetable.json");
