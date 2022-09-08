94f3206ff3dec152b0aa7675c94a1b574c34fbea
Hamzah Ahmed <hamzah@syedahmed.net>
2022-09-08 17:44:16 +1000

    feat: persist primary colour

--- hooks/useDynamicTheme.jsx
+++ hooks/useDynamicTheme.jsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 import contextualise from "../utils/contextualise";
 
 const [useDynamicTheme, DynamicThemeProvider] = contextualise(() => {
-  const [primary, setPrimary] = useState("blue");
+  const [primary, setPrimary] = useState(
+    localStorage.getItem("primary") || "blue"
+  );
 
-  return { primary, setPrimary };
+  return {
+    primary,
+    setPrimary: (color) => {
+      localStorage.setItem("primary", color);
+      setPrimary(color);
+    },
+  };
 });
 
 export { DynamicThemeProvider };
