3f4eb13984527a7d268517f146a55fbba6efef82
Hamzah Ahmed <hamzah@syedahmed.net>
2022-09-08 18:13:21 +1000

    feat: better colour selection ui

--- routes/Main/Settings/Appearance.jsx
+++ routes/Main/Settings/Appearance.jsx
@@ -5,13 +5,78 @@ import {
   Switch,
   useColorMode,
   Radio,
-  VStack,
+  SimpleGrid,
+  useRadio,
+  Box,
+  useRadioGroup,
 } from "@chakra-ui/react";
 import useDynamicTheme from "../../../hooks/useDynamicTheme";
 
+const PrimaryColour = (props) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        _checked={{
+          boxShadow: "outline",
+        }}
+        w={5}
+        h={5}
+        bg={props.value + ".500"}
+        rounded="full"
+      ></Box>
+    </Box>
+  );
+};
+
+const PrimaryColourPicker = () => {
+  const { primary, setPrimary } = useDynamicTheme();
+
+  const options = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "teal",
+    "blue",
+    "cyan",
+    "purple",
+    "pink",
+  ];
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    onChange: setPrimary,
+    value: primary,
+  });
+
+  const group = getRootProps();
+
+  return (
+    <FormControl display="flex">
+      <FormLabel mb="0">Primary Colour</FormLabel>
+      <SimpleGrid minChildWidth={5} spacing={5} {...group} w="full">
+        {options.map((value) => {
+          const radio = getRadioProps({ value });
+          return (
+            <PrimaryColour key={value} {...radio}>
+              {value}
+            </PrimaryColour>
+          );
+        })}
+      </SimpleGrid>
+    </FormControl>
+  );
+};
+
 export default () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { primary, setPrimary } = useDynamicTheme();
 
   return (
     <>
@@ -19,22 +84,7 @@ export default () => {
         <FormLabel mb="0">Dark mode</FormLabel>
         <Switch onChange={toggleColorMode} isChecked={colorMode === "dark"} />
       </FormControl>
-      <FormControl display="flex">
-        <FormLabel mb="0">Primary Colour</FormLabel>
-        <RadioGroup onChange={setPrimary} value={primary}>
-          <VStack spacing="24px" align={"left"}>
-            <Radio value="red">Red</Radio>
-            <Radio value="orange">Orange</Radio>
-            <Radio value="yellow">Yellow</Radio>
-            <Radio value="green">Green</Radio>
-            <Radio value="teal">Teal</Radio>
-            <Radio value="blue">Blue</Radio>
-            <Radio value="cyan">Cyan</Radio>
-            <Radio value="purple">Purple</Radio>
-            <Radio value="pink">Pink</Radio>
-          </VStack>
-        </RadioGroup>
-      </FormControl>
+      <PrimaryColourPicker />
     </>
   );
 };
