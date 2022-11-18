3404155434edf3718784486b2865f2d3f2a72320
Hamzah Ahmed <hamzah@syedahmed.net>
2022-11-18 14:14:06 +1100

    refactor: ts

--- src/errors/HTTPError.ts
+++ src/errors/HTTPError.ts
@@ -1,7 +1,9 @@
 import CustomError from "./CustomError";
 
 export default class HTTPError extends CustomError {
-  constructor(error_code) {
+  error_code: string;
+
+  constructor(error_code: string) {
     super(`HTTP Error: ${error_code}`);
     this.error_code = error_code;
   }
