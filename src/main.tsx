5d7c2ccbe3283d3b39efdcd15254f7c6e5b886a8
Hamzah Ahmed <hamzah@syedahmed.net>
2022-11-18 14:38:42 +1100

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -15,11 +15,16 @@ import { createStandaloneToast } from "@chakra-ui/toast";
 import reportWebVitals from "./reportWebVitals";
 import { sendToVercelAnalytics } from "./vitals";
 import useSettings, { SettingsProvider } from "./hooks/useSettings";
+import { inject } from "@vercel/analytics";
 
+// Redirect to new domain if using old domain
 if (window.location.host === "timetabl.vercel.app") {
   window.location.href = "https://www.timetabl.app";
 }
 
+/**
+ * QUERY CLIENT INITIALISATION
+ */
 const { ToastContainer, toast } = createStandaloneToast();
 
 export { toast };
@@ -48,6 +53,13 @@ const persister = createSyncStoragePersister({
   storage: window.localStorage,
 });
 
+/**
+ * RENDER ROOT
+ */
+
+/**
+ * A wrapper around the `ChakraProvider` component which generates the theme from settings and passes it to the the `ChakraProvider`.
+ */
 const ChakraWrapper = ({ ...props }) => {
   const { primary } = useSettings();
 
@@ -84,6 +96,17 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </Compose>
 );
 
+/**
+ * POST RENDER TASKS
+ */
+
+// Register service worker
 registerSW();
 
+// Report web vitals
 reportWebVitals(sendToVercelAnalytics);
+
+// Inject analytics
+inject({
+  beforeSend: (event) => ({ ...event, url: "https://www.timetabl.app" }),
+});
