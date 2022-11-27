98b4105dc678186929648f2e91842ef151fad372
Hamzah Ahmed <hamzah@syedahmed.net>
2022-11-27 18:21:38 +1100

    fix: add message handler for `skipwaiting`

--- sw.js
+++ sw.js
@@ -7,3 +7,9 @@ if (typeof is_vite_preview === "undefined") {
   const navigationRoute = new NavigationRoute(handler);
   registerRoute(navigationRoute);
 }
+
+addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
