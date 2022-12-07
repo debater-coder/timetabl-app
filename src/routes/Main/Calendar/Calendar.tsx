e915a01756ad6a9153959198c9be6fd2e936e83c
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-07 12:29:00 +1100

    Revert "Info in calendar (placeholder for now)"
    This reverts commit d30b108830326fac799ab3fc136932499eb21cb1.

--- src/routes/Main/Calendar/Calendar.tsx
+++ src/routes/Main/Calendar/Calendar.tsx
@@ -13,7 +13,7 @@ import {
 import { Calendar as CalendarIcon } from "phosphor-react";
 
 export const Calendar = () => {
-  console.log("In development");
+  console.log("calendar");
   return (
     <Flex w={"full"} h="full" direction={"column"} align="center">
       <Card bg="transparent" shadow="xl">
@@ -27,15 +27,13 @@ export const Calendar = () => {
               p={2}
               color="primary.400"
             />
-            Calendar <Badge>(coming soon)</Badge>
+            Calendar<Badge>Coming soon</Badge>
           </Heading>
         </CardHeader>
         <CardBody>
-          Calendar is currently in development. Please check back in a few days.
-          If you have any suggestions, <Link
-            href="https://github.com/debater-coder/timetabl-app/issues/new?assignees=debater-coder&labels=&template=feature_request.md&title=Feature%20Request%20for%20Calendar"
-            color={"primary.500"}
-          isExternal>please make a feature request</Link>.
+          Get a bird&apos;s eye view of your timetable, and see what&apos;s
+          coming. View your sport from Clipboard, see when you assignments are
+          due from Canvas and more.
         </CardBody>
       </Card>
     </Flex>
