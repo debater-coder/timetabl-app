1ae4ce137301eca4fc471439786232776c9229bc
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-07 12:31:31 +1100

    feat: cater to suggestions

--- src/routes/Main/Calendar/Calendar.tsx
+++ src/routes/Main/Calendar/Calendar.tsx
@@ -13,7 +13,6 @@ import {
 import { Calendar as CalendarIcon } from "phosphor-react";
 
 export const Calendar = () => {
-  console.log("calendar");
   return (
     <Flex w={"full"} h="full" direction={"column"} align="center">
       <Card bg="transparent" shadow="xl">
@@ -31,9 +30,17 @@ export const Calendar = () => {
           </Heading>
         </CardHeader>
         <CardBody>
-          Get a bird&apos;s eye view of your timetable, and see what&apos;s
-          coming. View your sport from Clipboard, see when you assignments are
-          due from Canvas and more.
+          Calendar is currently in development. Please check back in a few days.
+          Currently planned features:
+          <NodeList>
+            <Node>Get a bird&apos;s eye view of your timetable</Node>
+            <Node>View your sport from Clipboard</Node>
+            <Node>See when your Canvas assignments are due</Node>
+          </NodeList>
+          If you have any suggestions, <Link
+            href="https://github.com/debater-coder/timetabl-app/issues/new?assignees=debater-coder&labels=&template=feature_request.md&title=Feature%20Request%20for%20Calendar"
+            color={"primary.500"}
+          isExternal>please make a feature request</Link>.
         </CardBody>
       </Card>
     </Flex>
