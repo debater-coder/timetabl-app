823dc0ecd8a8d3e979ff2b0e695b881c3dec90af
Hamzah Ahmed <hamzah@syedahmed.net>
2022-12-11 18:37:38 +1100

    fix: stop `SidebarButton` animation from playing unless props change

--- src/components/Sidebar/SidebarButton.tsx
+++ src/components/Sidebar/SidebarButton.tsx
@@ -1,58 +1,61 @@
 import { Box, Flex, Text, useColorModeValue, useToken } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { IconWeight } from "phosphor-react";
+import React from "react";
 
-export default ({
-  name,
-  icon: Icon,
-  active,
-  sidebar,
-  mirrored = false,
-}: {
-  name: string;
-  icon: React.JSXElementConstructor<
-    React.PropsWithChildren<{
-      size: string | number;
-      mirrored: boolean;
-      weight: IconWeight;
-    }>
-  >;
-  active?: boolean;
-  sidebar?: boolean;
-  mirrored?: boolean;
-}) => {
-  const hoverColor = useColorModeValue("gray.100", "gray.700");
-  const [secondary] = useToken("colors", ["primary.300"]);
-  const MotionFlex = motion(Flex);
+export default React.memo(
+  ({
+    name,
+    icon: Icon,
+    active,
+    sidebar,
+    mirrored = false,
+  }: {
+    name: string;
+    icon: React.JSXElementConstructor<
+      React.PropsWithChildren<{
+        size: string | number;
+        mirrored: boolean;
+        weight: IconWeight;
+      }>
+    >;
+    active?: boolean;
+    sidebar?: boolean;
+    mirrored?: boolean;
+  }) => {
+    const hoverColor = useColorModeValue("gray.100", "gray.700");
+    const [secondary] = useToken("colors", ["primary.300"]);
+    const MotionFlex = motion(Flex);
 
-  return (
-    <Flex
-      _hover={{ bg: hoverColor }}
-      p={"12px"}
-      h={"64px"}
-      w="100%"
-      direction={"column"}
-      align={"center"}
-    >
-      <MotionFlex
-        justify={"center"}
-        bg={active ? secondary + "40" : undefined}
-        animate={{ width: active ? (sidebar ? "48px" : "56px") : "0px" }}
+    return (
+      <Flex
+        _hover={{ bg: hoverColor }}
+        p={"12px"}
+        h={"64px"}
+        w="100%"
+        direction={"column"}
         align={"center"}
-        borderRadius={"16px"}
-        h={"32px"}
       >
-        <Box minH={"24px"}>
-          <Icon
-            size="24px"
-            mirrored={mirrored}
-            weight={active ? "fill" : "duotone"}
-          />
-        </Box>
-      </MotionFlex>
-      <Text as={active ? "b" : "label"} fontSize={"xs"} mt="4px">
-        {name}
-      </Text>
-    </Flex>
-  );
-};
+        <MotionFlex
+          justify={"center"}
+          bg={active ? secondary + "40" : undefined}
+          animate={{ width: active ? (sidebar ? "48px" : "56px") : "0px" }}
+          align={"center"}
+          borderRadius={"16px"}
+          h={"32px"}
+        >
+          <Box minH={"24px"}>
+            <Icon
+              size="24px"
+              mirrored={mirrored}
+              weight={active ? "fill" : "duotone"}
+            />
+          </Box>
+        </MotionFlex>
+        <Text as={active ? "b" : "label"} fontSize={"xs"} mt="4px">
+          {name}
+        </Text>
+      </Flex>
+    );
+  }
+);
