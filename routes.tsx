1db21afd2b1b050aac9fb7b63f00349e2eef2379
Hamzah Ahmed <hamzah@syedahmed.net>
2022-12-13 15:31:06 +1100

    feat: add routes file

---
+++ routes.tsx
@@ -0,0 +1,41 @@
+import { Barcode, House, IconWeight } from "phosphor-react";
+
+export type TimetablRoute = {
+  path: string;
+  name: string;
+  icon: React.JSXElementConstructor<
+    React.PropsWithChildren<{
+      size: string | number;
+      mirrored: boolean;
+      weight: IconWeight;
+    }>
+  >;
+  mirrored: boolean;
+};
+
+export const routes: TimetablRoute[] = [
+  {
+    path: "/app",
+    name: "Home",
+    icon: House,
+    mirrored: false,
+  },
+  {
+    path: "/app/barcodes",
+    name: "Barcodes",
+    icon: Barcode,
+    mirrored: false,
+  },
+  {
+    path: "/app/announcements",
+    name: "Notices",
+    icon: House,
+    mirrored: false,
+  },
+  {
+    path: "/app",
+    name: "Home",
+    icon: House,
+    mirrored: false,
+  },
+];
