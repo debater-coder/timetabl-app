aac602187ada2e0d9ce6c1c7ed834d05933c8941
Hamzah Ahmed <hamzah@syedahmed.net>
2022-12-19 17:29:37 +1100

    feat: add formspree feedback from to Timetabl

---
+++ src/routes/Main/Feedback/Feedback.tsx
@@ -0,0 +1,52 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { useForm, ValidationError } from "@formspree/react";
+
+export const Feedback = () => {
+  const [state, handleSubmit] = useForm("meqdbykn");
+  if (state.succeeded) {
+    return <p>Thanks for the feedback!</p>;
+  }
+  return (
+    <Flex
+      as="form"
+      onSubmit={handleSubmit}
+      w="full"
+      pr={8}
+      maxW="2xl"
+      direction={"column"}
+      gap={4}
+    >
+      <FormControl>
+        <FormLabel>Email address</FormLabel>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="hello@timetabl.app"
+        />
+      </FormControl>
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+      <FormControl>
+        <FormLabel>Message</FormLabel>
+        <Textarea id="message" name="message" placeholder="I love this app!" />
+      </FormControl>
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+      <Button
+        type="submit"
+        isLoading={state.submitting}
+        loadingText="Submitting..."
+      >
+        Submit
+      </Button>
+    </Flex>
+  );
+};
