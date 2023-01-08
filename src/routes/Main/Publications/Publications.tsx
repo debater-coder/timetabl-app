5f49cc6590ecefcc6ef6aee043beb317688a1e85
Hamzah Ahmed <hamzah@syedahmed.net>
2023-01-09 03:26:12 +1100

    fix: add open pdf button fallback

--- src/routes/Main/Publications/Publications.tsx
+++ src/routes/Main/Publications/Publications.tsx
@@ -1,20 +1,32 @@
-import { chakra, Heading, Link } from "@chakra-ui/react";
+import { Button, chakra, Heading } from "@chakra-ui/react";
+import { useState } from "react";
 
-export default () => (
-  <>
-    <Heading fontFamily="Poppins, sans-serif" size="md">
-      Latest High Notes
-    </Heading>
-    <chakra.object
-      data="https://sbhs.co/hnpdf"
-      type="application/pdf"
-      width="100%"
-      height="100%"
-      p={8}
-    >
-      <Link isExternal href="https://sbhs.co/hnpdf">
-        Find PDF here
-      </Link>
-    </chakra.object>
-  </>
-);
+export default () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <>
+      <Heading fontFamily="Poppins, sans-serif" size="md">
+        Latest High Notes
+      </Heading>
+      <chakra.object
+        data="https://sbhs.co/hnpdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        p={8}
+      >
+        <Button
+          as="a"
+          mt={5}
+          href="https://sbhs.co/hnpdf"
+          isLoading={isLoading}
+          loadingText="Opening PDF..."
+          onClick={() => setIsLoading(true)}
+        >
+          Open PDF
+        </Button>
+      </chakra.object>
+    </>
+  );
+};
