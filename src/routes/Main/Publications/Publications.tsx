7ac349bf63e686eeace2f412f22fc6da87b414d4
Hamzah Ahmed <hamzah@syedahmed.net>
2023-01-11 13:18:30 +1100

    feat: introduce `PdfViewer` component

--- src/routes/Main/Publications/Publications.tsx
+++ src/routes/Main/Publications/Publications.tsx
@@ -1,32 +1,13 @@
-import { Button, chakra, Heading } from "@chakra-ui/react";
-import { useState } from "react";
+import { Heading } from "@chakra-ui/react";
+import PdfViewer from "../../../components/PdfViewer";
 
 export default () => {
-  const [isLoading, setIsLoading] = useState(false);
-
   return (
     <>
       <Heading fontFamily="Poppins, sans-serif" size="md">
         Latest High Notes
       </Heading>
-      <chakra.object
-        data="https://sbhs.co/hnpdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-        p={8}
-      >
-        <Button
-          as="a"
-          mt={5}
-          href="https://sbhs.co/hnpdf"
-          isLoading={isLoading}
-          loadingText="Opening PDF..."
-          onClick={() => setIsLoading(true)}
-        >
-          Open PDF
-        </Button>
-      </chakra.object>
+      <PdfViewer src="https://sbhs.co/hnpdf" />
     </>
   );
 };
