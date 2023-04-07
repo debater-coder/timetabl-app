410b2087b5f97433ee43a7ca78e4a87e62aa31b3
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-04-07 15:34:26 +1000

    style: cool gradients 😎

--- apps/client/src/components/App/App.tsx
+++ apps/client/src/components/App/App.tsx
@@ -1,11 +1,23 @@
 import Nav from "../Nav";
 import { Outlet } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
+import { Flex, useColorModeValue } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 
 export default () => {
   return (
-    <Flex direction={"column"} width={"100vw"} maxW="full" height={"100vh"}>
+    <Flex
+      direction={"column"}
+      width={"100vw"}
+      maxW="full"
+      height={"100vh"}
+      bgGradient={`linear-gradient(330deg, ${useColorModeValue(
+        "blue.100",
+        "blue.900"
+      )} 0, transparent 30%, transparent 70%, ${useColorModeValue(
+        "blue.100",
+        "blue.900"
+      )} 100%)`}
+    >
       <Nav />
       <Box mt={"100px"} />
       <Flex direction={"column"} w="full" h="full" maxH={"calc(100% - 100px)"}>
