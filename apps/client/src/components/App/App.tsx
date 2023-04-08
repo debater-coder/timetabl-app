392c259114da47f5fdc0f8be0dd1a7151297f46e
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-04-08 21:32:14 +1000

    fix: darker blue

--- apps/client/src/components/App/App.tsx
+++ apps/client/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import Nav from "../Nav";
 import { Outlet } from "react-router-dom";
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, useColorModeValue, useToken } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 
 export default () => {
@@ -10,10 +10,10 @@ export default () => {
       width={"100vw"}
       maxW="full"
       height={"100vh"}
-      bgGradient={`linear-gradient(0deg, transparent 30%, transparent 80%, ${useColorModeValue(
-        "primary.100",
-        "primary.900"
-      )} 100%)`}
+      bgGradient={`linear-gradient(0deg, transparent 20%, transparent 40%, ${useToken(
+        "colors",
+        useColorModeValue("primary.100", "primary.900")
+      )}44 100%)`}
     >
       <Nav />
       <Box mt={"100px"} />
