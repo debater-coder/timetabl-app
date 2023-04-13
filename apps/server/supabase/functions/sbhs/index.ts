c9b719f5dd06b8965703c5855f2491d26b19faac
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-04-13 18:58:51 +1000

    feat: get version

--- apps/server/supabase/functions/sbhs/index.ts
+++ apps/server/supabase/functions/sbhs/index.ts
@@ -1,18 +1,40 @@
 import { serve } from "server";
 import { corsHeaders } from "../_shared/cors.ts";
+import { z } from "zod";
 
 console.log(`Function "sbhs" up and running!`);
 
-serve((req) => {
+const apiVersion = "1.0.0";
+
+const requestPayloadSchema = z.discriminatedUnion("action", [
+  z.object({
+    action: z.literal("version"),
+  }),
+]);
+
+serve(async (req) => {
+  // Handle cors preflight
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
   }
 
-  const data = {
-    message: `Hello World!`,
-  };
+  // Validate payload
+  let payload;
+  try {
+    payload = requestPayloadSchema.parse(await req.json());
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 400,
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    });
+  }
 
-  return new Response(JSON.stringify(data), {
-    headers: { ...corsHeaders, "Content-Type": "application/json" },
-  });
+  // Handle request
+  switch (payload.action) {
+    case "version": {
+      return new Response(JSON.stringify(apiVersion), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+      });
+    }
+  }
 });
