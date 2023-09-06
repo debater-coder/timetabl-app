31f4cd6baff689cd4ec35aad6d15c0a378952606
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-09-06 10:41:46 +1000

    chore: get rid of week view

--- apps/client/src/routes/Main/Home/Home.tsx
+++ apps/client/src/routes/Main/Home/Home.tsx
@@ -1,10 +1,9 @@
 import { TabList, TabPanels, TabPanel, Tabs, Flex } from "@chakra-ui/react";
-import { CalendarBlank, Rows, SquaresFour } from "phosphor-react";
+import { CalendarBlank, SquaresFour } from "phosphor-react";
 import QuickLinks from "./QuickLinks";
 import Countdown from "./Countdown";
 import ViewTab from "./ViewTab";
 import DayTimetable from "./DayTimetable";
-import WeekTimetable from "./WeekTimetable";
 import CycleTimetable from "./Cycle";
 import { useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -20,18 +19,12 @@ export default function Home() {
       <Tabs variant={"unstyled"} mt={1}>
         <TabList>
           <ViewTab icon={CalendarBlank}>Day</ViewTab>
-          <ViewTab icon={Rows}>Week</ViewTab>
           <ViewTab icon={SquaresFour}>Cycle</ViewTab>
         </TabList>
         <TabPanels borderTop={"2px solid"} borderColor={"gray.500"}>
           <TabPanel>
             <ErrorBoundary fallback={<ErrorAlert />}>
               <DayTimetable />
-            </ErrorBoundary>
-          </TabPanel>
-          <TabPanel>
-            <ErrorBoundary fallback={<ErrorAlert />}>
-              <WeekTimetable />
             </ErrorBoundary>
           </TabPanel>
           <TabPanel>
