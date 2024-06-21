4dadfc6c5e5f991bdce457ee6be3df289f775af4
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-06-21 13:44:50 +1000

    delete ded files

--- apps/client/src/routes/Main/Home/Home.tsx
+++ apps/client/src/routes/Main/Home/Home.tsx
@@ -14,26 +14,7 @@ export default function Home() {
 
   return (
     <Flex direction={"column"} w={"full"} px={2} maxW={"1000px"}>
-      <QuickLinks />
-      <Countdown countdown={countdown} setCountdown={setCountdown} />
-      <Tabs variant={"unstyled"} mt={1}>
-        <TabList>
-          <ViewTab icon={CalendarBlank}>Day</ViewTab>
-          <ViewTab icon={SquaresFour}>Cycle</ViewTab>
-        </TabList>
-        <TabPanels borderTop={"2px solid"} borderColor={"gray.500"}>
-          <TabPanel>
-            <ErrorBoundary fallback={<ErrorAlert type="client" />}>
-              <DayTimetable />
-            </ErrorBoundary>
-          </TabPanel>
-          <TabPanel>
-            <ErrorBoundary fallback={<ErrorAlert type="client" />}>
-              <CycleTimetable />
-            </ErrorBoundary>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      Home is currently under maintenance bozos.
     </Flex>
   );
 }
