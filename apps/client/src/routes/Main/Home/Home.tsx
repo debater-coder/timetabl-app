ad5d9f9d56d9be09067a5011c02edd2f5a88fe63
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-06-21 13:45:26 +1000

    chore: delete unused imports

--- apps/client/src/routes/Main/Home/Home.tsx
+++ apps/client/src/routes/Main/Home/Home.tsx
@@ -1,16 +1,6 @@
-import ErrorAlert from "../../../components/ErrorAlert";
-import Countdown from "./Countdown";
-import CycleTimetable from "./Cycle";
-import DayTimetable from "./DayTimetable";
-import QuickLinks from "./QuickLinks";
-import ViewTab from "./ViewTab";
-import { TabList, TabPanels, TabPanel, Tabs, Flex } from "@chakra-ui/react";
-import { CalendarBlank, SquaresFour } from "phosphor-react";
-import { useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
-  const [countdown, setCountdown] = useState("00:00:00");
 
   return (
     <Flex direction={"column"} w={"full"} px={2} maxW={"1000px"}>
