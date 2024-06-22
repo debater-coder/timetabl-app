5bfaaf7daed88f7579a24fc72155a98fc90759ce
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-06-22 20:18:04 +1000

    prototype new ui

--- apps/client/src/routes/Main/Home/Home.tsx
+++ apps/client/src/routes/Main/Home/Home.tsx
@@ -1,10 +1,11 @@
-import { Flex, Heading, Skeleton } from "@chakra-ui/react";
+import { Button, Flex, IconButton, Select, Spacer } from "@chakra-ui/react";
 import { useQueries } from "@tanstack/react-query";
 import { useDataAmalgamator } from "../../../services/UserInterface";
 import ErrorAlert from "../../../components/ErrorAlert";
 import { detectErrorType } from "../../../components/ErrorAlert/ErrorAlert";
 import NotAvailable from "../../../components/NotAvailable";
 import Period from "./Period";
+import { ArrowLeft, ArrowRight } from "phosphor-react";
 
 export default function Home() {
   const dttQueries = useQueries({
@@ -20,13 +21,40 @@ export default function Home() {
   const errorType = detectErrorType(!!data, isError, isPaused);
 
   return (
-    <Flex direction={"column"} w={"full"} px={2} maxW={"1000px"}>
-
+    <Flex direction={"column"} w={"full"} px={2} maxW={"1000px"} gap={4}>
+      <Flex align="center" gap={2} w="full">
+        <Button size="sm" variant={"outline"} colorScheme="gray">Today</Button>
+        <IconButton
+              icon={<ArrowLeft />}
+              variant="outline"
+              colorScheme="gray"
+              isRound
+              aria-label="Previous day"
+              size={"sm"}
+            />
+            <IconButton
+              icon={<ArrowRight />}
+              variant="outline"
+              colorScheme="gray"
+              isRound
+              aria-label="Previous day"
+              size={"sm"}
+            />
+            <Button size="sm" colorScheme="gray">Mon, 24/06/2024, Wk 9B</Button>
+            <Spacer />
+            <Select w="fit-content" variant={"filled"} rounded="lg" size="sm">
+              <option value='option1'>Day</option>
+              <option value='option2'>Week</option>
+              <option value='option3'>Cycle</option>
+          </Select>
+            
+      </Flex>
       {/** Error state */}
       <ErrorAlert type={errorType} full />
-
       {/** Happy path */}
-      {(!errorType || data) && (data ? data.map(period => <Period period={period} isLoaded={!!data}  />) : undefined)}
+      <Flex w="full" direction={"column"}>
+        {(!errorType || data) && (data ? data.map(period => <Period period={period} isLoaded={!!data} />) : undefined)}
+      </Flex>
     </Flex>
     );
 }
--- apps/client/src/services/dataProviders/TestDataProvider/TestDataProvider.ts
+++ apps/client/src/services/dataProviders/TestDataProvider/TestDataProvider.ts
@@ -34,24 +34,24 @@ export class TestDataProvider implements DataProvider {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     queryFn: async (date: string) => [
       {
-        start: DateTime.fromISO("2023-09-20T09:00:00.000Z").toJSDate(),
-        end: DateTime.fromISO("2023-09-20T10:00:00.000Z").toJSDate(),
+        start: DateTime.fromISO("2023-09-20T09:00:00").toJSDate(),
+        end: DateTime.fromISO("2023-09-20T10:00:00").toJSDate(),
         name: { short: "8LAT1", long: "8 Latin 1" },
         teacher: [{ short: "JSM", long: "Mr Smith" }],
         location: [{ short: "203", long: "Room 203" }],
         color: "#79f2cc",
       },
       {
-        start: DateTime.fromISO("2023-09-20T10:05:00.000Z").toJSDate(),
-        end: DateTime.fromISO("2023-09-20T11:05:00.000Z").toJSDate(),
+        start: DateTime.fromISO("2023-09-20T10:05:00").toJSDate(),
+        end: DateTime.fromISO("2023-09-20T11:05:00").toJSDate(),
         name: { short: "8MA1", long: "8 Math 1" },
         teacher: [{ short: "JBA", long: "Ms Bash" }],
         location: [{ short: "103", long: "Room 103" }],
         color: "#f28579",
       },
       {
-        start: DateTime.fromISO("2023-09-20T11:10:00.000Z").toJSDate(),
-        end: DateTime.fromISO("2023-09-20T11:40:00.000Z").toJSDate(),
+        start: DateTime.fromISO("2023-09-20T11:10:00").toJSDate(),
+        end: DateTime.fromISO("2023-09-20T11:40:00").toJSDate(),
         name: { short: "RCS", long: "Recess" },
       },
     ],
@@ -63,7 +63,7 @@ export class TestDataProvider implements DataProvider {
   notices = {
     queryFn: async () => [
       {
-        date: DateTime.fromISO("2023-09-20T00:00:00.000Z").toJSDate(),
+        date: DateTime.fromISO("2023-09-20T00:00:00").toJSDate(),
         title: "Test Notice",
         content: "This is a test notice.",
         content_encoding: "markdown" as const,
@@ -71,7 +71,7 @@ export class TestDataProvider implements DataProvider {
         author: "Test Author",
       },
       {
-        date: DateTime.fromISO("2024-04-01T00:00:00.000Z").toJSDate(),
+        date: DateTime.fromISO("2024-04-01T00:00:00").toJSDate(),
         title: "Test Notice 2",
         content: `Certainly! Let's delve into the arcane and invent some truly one-of-a-kind Latin species names for these tech errors:
 
